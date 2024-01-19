1d73bf4c160afdca64e86d5acd3a27ae2e079204
JASON SELVAKUMAR <jasonselvakumarw.21csd@kongu.edu>
2024-01-19 18:00:56 +0530

    Railway Reservation

---
+++ pnrstatus.php
@@ -0,0 +1,96 @@
+<?php 
+session_start();
+$conn = mysqli_connect("localhost","root","","railway");
+if(!$conn){  
+	echo "<script type='text/javascript'>alert('Database failed');</script>";
+  	die('Could not connect: '.mysqli_connect_error());  
+}
+if (isset($_POST['submit']))
+{
+$pnr=$_POST['pnr'];
+$sql = "SELECT t_status FROM tickets WHERE PNR= '$pnr'";
+$result = mysqli_query($conn, $sql);
+$row = mysqli_fetch_assoc($result);
+	if($row==NULL)	echo "<script type='text/javascript'>alert('PNR not found');</script>";
+	else echo "<script type='text/javascript'>alert('Your status is ' +'$row[t_status]');</script>";	
+}
+if (isset($_POST['cancel'])) {
+    $pnr = $_POST['pnr'];
+    
+    // Prepare the SQL statement with a parameter
+    $sql = "DELETE FROM tickets WHERE PNR = ?";
+    $stmt = mysqli_prepare($conn, $sql);
+    
+    if ($stmt) {
+        // Bind the parameter to the statement
+        mysqli_stmt_bind_param($stmt, "s", $pnr);
+        
+        // Execute the statement
+        if (mysqli_stmt_execute($stmt)) {
+            echo "<script type='text/javascript'>alert('Your ticket has been cancelled');</script>";
+        } else {
+            echo "<script type='text/javascript'>alert('Cancellation failed');</script>";
+        }
+        
+        // Close the statement
+        mysqli_stmt_close($stmt);
+    }
+}
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PNR Status</title>
+	<LINK REL="STYLESHEET" HREF="STYLE.CSS">
+	<style type="text/css">
+		#pnr	{
+			font-size: 20px;
+			background-color: white;
+			width: 500px;
+			height: 300px;
+			margin: auto;
+			border-radius: 25px;
+			border: 2px solid blue; 
+			margin: auto;
+  			position: absolute;
+  			left: 0; 
+  			right: 0;
+  			padding-top: 40px;
+  			padding-bottom:20px;
+  			margin-top: 130px;
+ 
+		}
+		html { 
+		  background: url(img/bg7.jpg) no-repeat center center fixed; 
+		  -webkit-background-size: cover;
+		  -moz-background-size: cover;
+		  -o-background-size: cover;
+		  background-size: cover;
+		}
+		#pnrtext	{
+			padding-top: 20px;
+		}
+	</style>
+</head>
+<body>
+<?php
+include("header.php"); ?>
+<center>
+	<div id="pnr">Check your PNR status here:<br/><br/>
+	<form method="post" name="pnrstatus" action="pnrstatus.php">
+	<div id="pnrtext"><input type="text" name="pnr" size="30" maxlength="10" placeholder="Enter PNR here"></div>
+	<br/><br/>
+	<input type="submit" name="submit" value="Check here!" class="button" id="submit"><br/><br/>
+	<?php  
+		if(isset($_SESSION['user_info'])){
+			echo '<form action="pnrstatus.php" method="post"><input type="submit" class="button" value="Cancel your ticket!" name="cancel" id="cancel"/></form>';
+        }
+		else
+			echo '<A HREF="register.php">Login/Register</A>';
+		?>
+	</form>
+	</div>
+</center>
+</body>
+</html>
